46379edc55c943c8729d5f280adbbc93f8be2f2d
gregcline <gregrcline@gmail.com>
2016-08-06 17:48:50 -0400

    Update router.ex

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Cookbook.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/recipes", RecipeController, :index
   end
 
   # Other scopes may use custom stacks.
