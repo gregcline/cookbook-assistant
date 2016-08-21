e10c15c69ecdc4766172b1fc34ff5b20f0362ec3
Greg Cline <gregrcline@gmail.com>
2016-08-21 19:35:32 -0400

    Update the homepage

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule Cookbook.PageController do
   use Cookbook.Web, :controller
+  alias Cookbook.Recipe
+  alias Cookbook.Repo
+  import Ecto.Query, only: [from: 2]
 
   def index(conn, _params) do
-    render conn, "index.html"
+    recipes = Repo.all(top_ten)
+    render conn, "index.html", recipes: recipes
+  end
+
+  def top_ten do
+    from r in Recipe,
+      limit: 10,
+      order_by: [desc: :inserted_at]
   end
 end
